2d28167b4988969c91946bcd46a4d61a45552433
Mohit Aggarwal <agg.mohit@gmail.com>
2014-09-24 14:30:52 +0530

    Revert "Revert "Unified link generation in single mapper.""
    This reverts commit 49d122957a4e2ba4704bf1cee4025d324f4ecb15.

---
+++ wiki-hadoop/src/main/java/com/magg/wiki/mapreduce/PageLinksDriver.java
@@ -0,0 +1,45 @@
+package com.magg.wiki.mapreduce;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.ArrayWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import com.magg.wiki.model.LinksWrittable;
+
+public class PageLinksDriver {
+	
+	public static void main(String[] args) throws Exception {
+		Configuration conf = new Configuration();
+		conf.set("xmlinput.start", "<page>");
+		conf.set("xmlinput.end", "</page>");
+		
+		Job job = new Job(conf);
+		job.setJarByClass(PageLinksDriver.class);
+		job.setJobName("Links");
+		
+		job.setInputFormatClass(XmlInputFormat.class);
+		
+		job.setMapperClass(PageLinksMapper.class);
+		job.setReducerClass(PageLinksReducer.class);
+		
+		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(LinksWrittable.class);
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(ArrayWritable.class);
+
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+
+}
